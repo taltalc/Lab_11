d25a1445072cf7f1df998c366bccdbcea5cf744c
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-28 15:53:21 +0500

    Добавьте файлы проекта.

---
+++ equation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation;
+            equation.k = 0;
+            equation.b = 0;
+            equation.Root();
+            Console.ReadKey();
+        }
+
+    }
+    struct Equation
+    {
+
+        public double k;
+
+
+        public double b;
+
+
+
+        public void Root()
+        {
+            double x = 0;
+
+            x = (0 - b) / k;
+
+            if (Double.IsInfinity(x)||Double.IsNaN(x))
+            {
+
+                Console.Write("Деление на ноль!");
+            }
+            else
+            {
+                double result = x;
+                Console.WriteLine("Решение уравнения: {0:f2}", result);
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
